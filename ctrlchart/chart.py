4aa85d651d5b8b37aa54e246cb2b413765c6e767
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2025-05-16 16:29:14 +0200

    🚧 work in progress

---
+++ ctrlchart/chart.py
@@ -0,0 +1,11 @@
+class EWMAChart:
+    def __init__(self, m0:float, m1:float, mp1:float):
+        self.m0 = m0
+        self.m1 = m1
+        self.mp1 = mp1
+
+
+    def fit(self, X, y):
+
+    def transform(self, ):
+
